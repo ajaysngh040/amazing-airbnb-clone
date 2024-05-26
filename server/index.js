27002e7b5af59c1092547b52c31fba8d62025e3f
Ajay Singh <ajaysngh040@gmail.com>
2024-05-26 22:20:37 +0530

    hashsync updated

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ function getUserDataFromReq(req) {
 app.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const hashedPassword = bcrypt.hashsync(password, bcryptSalt);
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
     const userDoc = await User.create({
       name,
       email,
