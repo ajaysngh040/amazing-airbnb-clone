13743835c0d6b129946fb27e8da1eb31735fee37
Ajay Singh <ajaysngh040@gmail.com>
2024-05-26 11:31:22 +0530

    PlaceImg updated

--- client/src/components/PlaceImg.jsx
+++ client/src/components/PlaceImg.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/prop-types */
 import Image from "../components/Image";
 
-export default function PlaceImg({ place, index = 0, className = null }) {
-  if (!place.photos?.length) {
-    return "";
+export default function PlaceImg({
+  place,
+  index = 0,
+  className = "object-cover",
+}) {
+  if (!place?.photos?.length) {
+    return null; // Returning null instead of an empty string for better React practices
   }
-  if (!className) {
-    className = "object-cover";
-  }
-  return <Image className={className} src={place.photos[index]} alt="" />;
+
+  return (
+    <Image
+      className={className}
+      src={place.photos[index]}
+      alt={`Photo ${index + 1}`}
+    />
+  );
 }
