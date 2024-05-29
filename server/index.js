7ca560f54a3148a9f18009ad8ee6a3fb402cba9e
Ajay Singh <ajaysngh040@gmail.com>
2024-05-29 22:00:07 +0530

    port updated again

--- server/index.js
+++ server/index.js
@@ -271,5 +271,5 @@ app.delete("/places/:id", async (req, res) => {
 // Start the server
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT || process.env.PORT}`);
 });
