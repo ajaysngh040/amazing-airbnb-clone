abd768bde32f1011fcb0b08e5ca9e2e8bbd906e4
Ajay Singh <ajaysngh040@gmail.com>
2024-10-13 23:44:39 +0530

    secure to false

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,13 +34,9 @@ exports.login = async (req, res) => {
 
     res
       .cookie("token", token, {
-        httpOnly: true, // Prevent client-side JS from accessing the cookie
-        secure: process.env.NODE_ENV === production, // Enable HTTPS only in production
-        sameSite: process.env.NODE_ENV === production ? None : Lax, // Set SameSite None in production
-        domain:
-          process.env.NODE_ENV === production
-            ? "https://amazing-airbnb-clone.vercel.app"
-            : undefined, // Domain only for production
+        httpOnly: true, // CHANGE THIS TO FALSE IN DEVELOPMENT
+        secure: false, // Set secure cookie in production
+        sameSite: "None", // SameSite None for cross-site in production
       })
       .json({ message: "Login successful" });
   } catch (error) {
