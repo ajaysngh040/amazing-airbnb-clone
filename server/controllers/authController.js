21929e53a5847265b70814b4d1616da569659bb7
Ajay Singh <ajaysngh040@gmail.com>
2024-10-13 23:23:18 +0530

    Cookies updated PRODUCTION

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,10 +35,10 @@ exports.login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true, // Prevent client-side JS from accessing the cookie
-        secure: process.env.NODE_ENV === "production", // Enable HTTPS only in production
-        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Set SameSite None in production
+        secure: process.env.NODE_ENV === production, // Enable HTTPS only in production
+        sameSite: process.env.NODE_ENV === production ? "None" : "Lax", // Set SameSite None in production
         domain:
-          process.env.NODE_ENV === "production"
+          process.env.NODE_ENV === production
             ? "https://amazing-airbnb-clone.vercel.app"
             : undefined, // Domain only for production
       })
